5631806437c8cb36cab456873b5a7fc6a76e6db5
Kevin Campusano <kcampusano@endpoint.com>
2020-03-09 20:58:18 -0400

    Configure babel

--- final/webpack.config.js
+++ final/webpack.config.js
@@ -7,4 +7,9 @@ module.exports = {
         filename: "index.js",
         path: path.resolve(__dirname, "dist"),
     },
+    module: {
+        rules: [
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+        ]
+    }
 };
